3ee53569818f957f154ee0f7a9e77c687cf61faf
Jin <jinpark1504@gmail.com>
2020-04-22 23:04:55 -0400

    make a contact list

--- App.js
+++ App.js
@@ -1,12 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, FlatList,SectionList } from 'react-native';
+import Constants from 'expo-constants';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import contacts, {compareNames} from './contacts';
+import Row from './Row';
+
+export default class App extends React.Component {
+  state = {
+    showContacts: false,
+    contacts: contacts
+  }
+
+  toggleContacts = () => {
+    this.setState(prevState => ({
+      showContacts: !prevState.showContacts
+    }))
+  }
+
+  sort = () => {
+    this.setState(prevState => ({
+      contacts:  [...prevState.contacts].sort(compareNames)
+    }))
+  }
+
+  // key is automatically generated
+  // item: { name: String, phone: String, key: number }
+  // renderItem = obj => <Row name={obj.item.name} phone={obj.item.phone} key={obj.item.key} />
+  // renderItem = obj => <Row {...(obj.item)} />
+  renderItem = ({item}) => <Row {...item} />
+  renderSectionHeader = obj => <Text>{obj.section.title}</Text>
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="toggle contacts" onPress={this.toggleContacts} />
+        <Button title="sort" onPress={this.sort} />
+        {this.state.showContacts && (
+          // using ScrollView
+          // <ScrollView>
+          //   {contacts.map(contact => 
+          //     // <Row key={c.key} name={c.name} phone={c.phone} />
+          //     <Row {...contact} />
+          //   )}
+          // </ScrollView>
+
+          // using FlatList
+          // <FlatList
+          //   renderItem={this.renderItem}
+          //   data={this.state.contacts}
+          // />
+
+          // using SectionList
+          <SectionList
+            renderItem={this.renderItem}
+            renderSectionHeader={this.renderSectionHeader}
+            sections={[{
+              title: 'A',
+              data: this.state.contacts
+            }]}
+          />
+        )}
+        
+      </View>
+    );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +71,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
 });
---
+++ contacts.js
@@ -0,0 +1,31 @@
+const NUM_CONTACTS = 100
+
+const firstNames = ['Liam','Emma','Noah','Olivia','William','Ava','James','Isabella','Oliver','Sophia','Benjamin','Charlotte','Elijah','Mia','Lucas','Amelia','Mason','Harper','Logan','Evelyn','Henry','Aria','Qwen','Lily','Sebastian','Levi','Ellie','Nathan','Samantha','Caleb']
+
+const lastNames = ['Smith','Hones','Brown','Johnson','Williams','Miller','Taylor','Wilson','Davis','White','Clark','Hall','Thomas','Thompson','Moore','Scott','Young','Jackson','Adams','Tryniski','Green','Evans','King','Baker','John','Harris','Roberts','Campbell','James','Carter','Murphy']
+
+// generate a random number between min and max
+const rand = (max, min = 0) => Math.floor(Math.random() * (max - min + 1)) + min
+
+// generate a name
+const generateName = () => `${firstNames[rand(firstNames.length - 1)]} ${lastNames[rand(lastNames.length - 1)]}`
+
+// generate a phone number
+const generatePhoneNum = () => `${rand(999, 100)}-${rand(999,100)}-${rand(9999,1000)}`
+
+// create a person
+const createContact = () => ({name: generateName(), phone: generatePhoneNum()})
+
+// compare two contacts for alpabetizing
+export const compareNames = (contact1, contact2) => contact1.name > contact2.name
+
+// add keys to based on index
+// const addKeys = (contact, key) => ({
+//     key: key,
+//     name: contact.name,
+//     phone: contact.phone,
+// })
+const addKeys = (contact, key) => ({key, ...contact})
+
+// create an array of length NUM_CONTACTS and alphabetize by name
+export default Array.from({length: NUM_CONTACTS}, createContact).map(addKeys)
