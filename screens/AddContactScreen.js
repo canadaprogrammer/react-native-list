5173a20403f3e4aa65ba46467b709d66564178a7
Jin <jinpark1504@gmail.com>
2020-05-16 00:18:59 -0400

    make HOCs for adding contact

--- screens/AddContactScreen.js
+++ screens/AddContactScreen.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react'
 import AddContactForm from '../AddContactForm'
-import store from '../redux/store'
+// import store from '../redux/store'
 import {addContact} from '../redux/actions'
+import {connect} from 'react-redux'
 
-export default class AddContactScreen extends Component {
+// export default class AddContactScreen extends Component {
+
+class AddContactScreen extends Component {
     static navigationOptions = {
         headerTitle: 'Add Contact',
     }
     handleSubmit = formState => {
         // this.props.screenProps.addContact(formState)
 
-        store.dispatch(addContact({name: formState.name, phone: formState.phone}))
+        // store.dispatch(addContact({name: formState.name, phone: formState.phone}))
+
+        this.props.addContact({name: formState.name, phone: formState.phone})
         this.props.navigation.navigate('ContactList')
     }
 
@@ -18,3 +23,5 @@ export default class AddContactScreen extends Component {
         return <AddContactForm onSubmit={this.handleSubmit}/>
     }
 }
+
+export default connect(null, {addContact: addContact})(AddContactScreen)
