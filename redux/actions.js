c46a02cceb940de8c3673c1569221d291b1d4576
Jin <jinpark1504@gmail.com>
2020-05-16 12:39:01 -0400

    apply eslint

--- redux/actions.js
+++ redux/actions.js
@@ -8,23 +8,23 @@ export const LOG_IN_FULFILLED = 'LOG_IN_FULFILLED'
 export const LOG_IN_REJECTED = 'LOG_IN_REJECTED'
 
 // action creators
-export const updateUser = update => ({
-    type: UPDATE_USER,
-    payload: update
+export const updateUser = (update) => ({
+  type: UPDATE_USER,
+  payload: update,
 })
 
-export const addContact = newContact => ({
-    type: UPDATE_CONTACT,
-    payload: newContact
+export const addContact = (newContact) => ({
+  type: UPDATE_CONTACT,
+  payload: newContact,
 })
 
 // async action creator
-export const logInUser = (username, password) => async dispatch => {
-    dispatch({type: LOG_IN_SENT})
-    try {
-        const token = await login(username, password)
-        dispatch({type: LOG_IN_FULFILLED, payload: token})
-    } catch(err) {
-        dispatch({type: LOG_IN_REJECTED, payload: err.message})
-    }
+export const logInUser = (username, password) => async (dispatch) => {
+  dispatch({type: LOG_IN_SENT})
+  try {
+    const token = await login(username, password)
+    dispatch({type: LOG_IN_FULFILLED, payload: token})
+  } catch (err) {
+    dispatch({type: LOG_IN_REJECTED, payload: err.message})
+  }
 }
--- redux/reducer.js
+++ redux/reducer.js
@@ -1,31 +1,32 @@
+/* eslint-disable import/order */
 import {combineReducers} from 'redux'
-import {UPDATE_CONTACT, UPDATE_USER, LOG_IN_SENT, LOG_IN_FULFILLED, LOG_IN_REJECTED} from './actions'
+import {UPDATE_CONTACT, UPDATE_USER, LOG_IN_FULFILLED, LOG_IN_REJECTED} from './actions'
 
-const marge = (prev, next) => Object.assign({}, prev, next)
+const marge = (prev, next) => ({...prev, ...next})
 
 const contactReducer = (state = [], action) => {
-    if (action.type === UPDATE_CONTACT) return [...state, action.payload]
-    return state
+  if (action.type === UPDATE_CONTACT) return [...state, action.payload]
+  return state
 }
 
 const userReducer = (state = {}, action) => {
-    switch (action.type) {
-        case UPDATE_USER:
-            return marge(state, action.payload)
-        case UPDATE_CONTACT:
-            return marge(state, {prevContact: action.payload})
-        case LOG_IN_FULFILLED:
-            return marge(state, {token: action.payload})
-        case LOG_IN_REJECTED:
-            return marge(state, {loginErr: action.payload})
-        default:
-            return state
-    }
+  switch (action.type) {
+    case UPDATE_USER:
+      return marge(state, action.payload)
+    case UPDATE_CONTACT:
+      return marge(state, {prevContact: action.payload})
+    case LOG_IN_FULFILLED:
+      return marge(state, {token: action.payload})
+    case LOG_IN_REJECTED:
+      return marge(state, {loginErr: action.payload})
+    default:
+      return state
+  }
 }
 
 const reducer = combineReducers({
-    user: userReducer,
-    contacts: contactReducer
+  user: userReducer,
+  contacts: contactReducer,
 })
 
 export default reducer
